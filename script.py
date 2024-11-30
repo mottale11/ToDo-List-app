403278054fa4f76cd8302367f749e2d932cb4baa
Moses Mwai <148555858+mottale11@users.noreply.github.com>
2024-11-30 11:24:14 +0300

    script.py

--- script.py
+++ script.py
@@ -1,13 +1,18 @@
 # base code
-todo_list = ["watch movie", "take a walk", "submit project"
-new_task = input("Enter the task: ")
-todo_list.append(new_task)
-print(f"{new_task} added")
-if len(todo_list) == 0:
-  print("Your ToDo list is empty")
-else:
-  len(todo_list) > 0
-  index = 1
-  for task in todo_list:
-    print(f"{index}. {task}")
-    index +=1
+
+todo_list = ["Watch a movie", "Take a walk", "Submit project work"]
+
+while True:
+  todo_list = ["Watch a movie", "Take a walk", ]
+  
+  # Ask users to add new task
+  new_task = input("Enter the task: ")
+  todo_list.append(new_task)     # adding the new task
+  print(f"{new_task} added")
+  if len(todo_list) == 0:
+    print("Your ToDo list is empty")
+  else:
+    index = 1
+    for task in todo_list:
+      print(f"{index}. {task}")
+      index += 1
