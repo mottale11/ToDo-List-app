0ec6387a398cca0aaa96625edb52afa77cbc17e6
Moses Mwai <148555858+mottale11@users.noreply.github.com>
2024-12-03 11:03:53 +0300

    script.py

--- script.py
+++ script.py
@@ -1,14 +1,8 @@
 # base code
 
-todo_list = ["Watch a movie", "Take a walk", "Submit project work"]
+todo_list = []
 
 while True:
-  todo_list = ["Watch a movie", "Take a walk", ]
-  
-  # Ask users to add new task
-  new_task = input("Enter the task: ")
-  todo_list.append(new_task)     # adding the new task
-  print(f"{new_task} added")
   if len(todo_list) == 0:
     print("Your ToDo list is empty")
   else:
@@ -16,3 +10,25 @@ while True:
     for task in todo_list:
       print(f"{index}. {task}")
       index += 1
+
+
+  print("Options:")
+  print("1) Add Task")
+  print("2) Remove Task")
+  print("3) Quit")
+  
+  choice = input("Enter choice (1, 2, or 3): ")
+
+  if choice == "1":
+    print(f"Adding task")
+    new_task = input("Enter you task: ")
+    todo_list.append(new_task)
+    print("Your task has been added.")
+  elif choice == "2":
+    if len(todo_list) == 0:
+      print("ToDo list is empty")
+    elif len(todo_list) > 0:
+      todo_list.pop()
+  elif choice == "3":
+    print("Quitting")
+    break
